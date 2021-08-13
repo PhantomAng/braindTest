143b37ffbe24b8942b90ccb4362fb73ecb64f61b
PhantomAng <logvinov.daniil@gmail.com>
2021-08-13 09:13:14 +0300

    Последнии изменения

--- bitrix/components/phantom/centralbank/class.php
+++ bitrix/components/phantom/centralbank/class.php
@@ -1,34 +1,41 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
+use Phantom\Local\Helper;
+
 class CurrencyExchangeRate extends CBitrixComponent
 {
-    function currencyRate($arParams)
+    protected function currencyRate()
     {
+        $help = new Helper();
         $date = new DateTime();
         $curDate = $date->format('d/m/Y');
-        $val = simplexml_load_file("http://www.cbr.ru/scripts/XML_daily.asp?date_req=$curDate&d=0");
-
-
-        if(!is_string($val)) {
-            foreach ($arParams['CURRENCY'] as $param) {
-                foreach ($val->Valute as $vt) {
-                    if (trim((string)$vt['ID']) == trim((string)$param)) {
-                        $temp = (float)$vt->Value;
-                        $temp = round($temp, 4);
-                        $arResult['RATE'][$param] = $temp;
-                        $arResult['NAME'][$param] = (string)$vt->Name;
+        if($this->startResultCache($this->arParams['CACHE_TIME'])) {
+            $val = simplexml_load_file("http://www.cbr.ru/scripts/XML_daily.asp?date_req=$curDate&d=0");
+            if (!is_string($val)) {
+                foreach ($this->arParams['CURRENCY'] as $param) {
+                    foreach ($val->Valute as $vt) {
+                        if (trim((string)$vt['ID']) == trim((string)$param)) {
+                            $temp = (float)$vt->Value;
+                            $temp = round($temp, 4);
+                            $this->arResult['RATE'][$param] = $help->DeclensionOfWords($temp, array('рубль', 'рубля', 'рублей'));
+                            $this->arResult['NAME'][$param] = (string)$vt->Name;
+                        }
                     }
                 }
             }
+            else{
+                $this->abortResultCache();
+            }
         }
-        else $arResult = $val;
-
-        return $arResult;
+        else{
+            $this->abortResultCache();
+        }
     }
+
     public function executeComponent()
     {
-        $this->arResult = array_merge($this->arResult, $this->currencyRate($this->arParams));
+        $this->currencyRate();
         $this->includeComponentTemplate();
     }
 }
--- bitrix/components/phantom/centralbank/templates/.default/template.php
+++ bitrix/components/phantom/centralbank/templates/.default/template.php
@@ -5,17 +5,12 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); ?>
     <div class="row">
         <div class="col">
             <h2>Курс валют:</h2>
-                <?if($arResult != false):
+                <?
                 foreach ($arResult['RATE'] as $key => $rate):?>
                     <div class="col-3">
-                        <p><span style="font-weight: bolder"><?echo $arResult['NAME'][$key]?>:</span> <? echo $rate?> руб.</p>
+                        <p><span style="font-weight: bolder"><?echo $arResult['NAME'][$key]?>:</span> <? echo $rate?></p>
                     </div>
-                <?endforeach; endif;?>
-                <?if($arResult == false):?>
-                    <div class="col-3">
-                        <p>Сервис временно не доступен</p>
-                    </div>
-                <?endif;?>
+                <?endforeach;?>
             </div>
     </div>
 </div>
